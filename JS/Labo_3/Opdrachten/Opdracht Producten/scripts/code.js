9902c4b60cabac9a2b73d7cf5b525feceb73192c
TimSchmit <Schmit.Tim@outlook.com>
2023-03-10 13:42:00 +0100

    oef producten toegevoegt

--- JS/Labo_3/Opdrachten/Opdracht Producten/scripts/code.js
+++ JS/Labo_3/Opdrachten/Opdracht Producten/scripts/code.js
@@ -1,9 +1,32 @@
 const setup = () => {
-    console.log("setup");
-    let products = document.getElementsByClassName("aantal")
-    for (let i = 0;i<products.length;i++){
+    let button = document.getElementById("herberekenen");
+    button.addEventListener("click", update)
+}
 
+const update = () => {
+    let txtBTWs = document.getElementsByClassName('BTW');
+    let txtAantallen = document.getElementsByClassName('aantal');
+    let txtPrijzen = document.getElementsByClassName('prijs');
+    let txtSubtotalen = document.getElementsByClassName('subtotaal');
+    let totaal = 0.0;
+
+    for (let i = 0;i<txtAantallen.length;i++){
+        let aantal = parseFloat(txtAantallen[i].value);
+        let prijs = parseFloat(txtPrijzen[i].innerHTML);
+        let btw = parseFloat(txtBTWs[i].innerHTML);
+
+        let subtotaal = (prijs + (prijs * (btw/100))) * aantal;
+
+        subtotaal = subtotaal.toFixed(2);
+        subtotaal = parseFloat(subtotaal);
+
+        txtSubtotalen[i].innerHTML = subtotaal + " EUR";
+        totaal += subtotaal;
     }
 
+    totaal = totaal.toFixed(2);
+    totaal = parseFloat(totaal);
+
+    document.getElementById("totaal").innerHTML = totaal + " EUR"
 }
 window.addEventListener("load", setup);
